ccc3a074f13895d467ed01b5cf75f5b935aebe8c
jaydevk25 <jaydevkumarjk25@gmail.com>
2025-10-15 17:50:34 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['cdn.prod.website-files.com'],
+  },
+  env: {
+    GROQ_API_KEY: process.env.GROQ_API_KEY,
+  },
+}
+
+module.exports = nextConfig
